8863532d15910d252d8a36dfea214a7316452826
jake <jst4nt0n@gmail.com>
2020-03-26 17:30:04 -0400

    needed the correct key

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,9 @@ echo "Installing 'node-cmd'"
 npm install node-cmd
 echo "Installing 'node-ssh'"
 npm install node-ssh
+echo "Installing 'fs'"
+npm install fs
+echo "Installing 'path'"
+npm install path
 echo "Running 'deploy.js'"
 node deploy.js
